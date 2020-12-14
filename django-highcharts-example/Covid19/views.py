e2feaf06e995b285db7d3806d509e2329f1cfbf3
seokhyeon <seokhyeon116@naver.com>
2020-12-15 03:01:16 +0900

    add 12-15

--- django-highcharts-example/Covid19/views.py
+++
@@ -1,9 +0,0 @@
-import json
-
-from django.db.models import Count, Q
-from django.shortcuts import render
-from django.http import JsonResponse
-
-
-def home(request):
-    return render(request, 'home.html')
